cfbd8965c0b5ce361655cb86e21cd47bcda79802
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-02-14 16:04:26 +0100

    roslaunch2: allow multiple <remap> arguments at once

--- src/roslaunch2/remapable.py
+++ src/roslaunch2/remapable.py
@@ -9,15 +9,23 @@ class Remapable(GeneratorBase):
     """
     def __init__(self):
         GeneratorBase.__init__(self)
-        self.from_name = None
-        self.to_name = None
+        self.from_name = list()
+        self.to_name = list()
 
     def remap(self, from_name, to_name):
-        self.from_name = from_name
-        self.to_name = to_name
+        if from_name and to_name:
+            self.from_name.append(from_name)
+            self.to_name.append(to_name)
+        else:
+            raise ValueError('Parameters cannot be empty.')
+
+    def unmap(self):
+        self.from_name = list()
+        self.to_name = list()
 
     def generate(self, root, machines):
         if self.from_name and self.to_name:
-            elem = etree.SubElement(root, 'remap')
-            GeneratorBase.to_attr(elem, 'from', self.from_name, str)
-            GeneratorBase.to_attr(elem, 'to', self.to_name, str)
+            for fn, tn in zip(self.from_name, self.to_name):
+                elem = etree.SubElement(root, 'remap')
+                GeneratorBase.to_attr(elem, 'from', fn, str)
+                GeneratorBase.to_attr(elem, 'to', tn, str)
