47001a573e0578b89b6d05a7bfeb7fef007bfaaf
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-02-14 16:04:51 +0100

    roslaunch2: provide missing silent_remove(file) implementation in utils

--- src/roslaunch2/utils.py
+++ src/roslaunch2/utils.py
@@ -1,5 +1,7 @@
 import string
 import random
+import os
+
 
 def anon(length=6):
     """
@@ -25,3 +27,20 @@ def anon(length=6):
     if length <= 1:
         raise NameError("ROS names should be longer than 1 character.")
     return "{0}{1}".format(id_gen(1, chars=string.ascii_uppercase + string.ascii_lowercase), id_gen(length - 1))
+
+
+def silent_remove(path):
+    """
+    Remove the file given by path and do not fail if the file does not exist.
+
+    :param path: path to file which should be deleted silently
+    :return: Returns True if the file was removed, False otherwise.
+    """
+    try:
+        os.remove(path)
+        return True
+    except OSError as ose:
+        if ose.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # re-raise exception if a different error occurred
+        else:
+            return False
