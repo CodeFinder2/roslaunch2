44f41c8b5ef472d9d103e88b2260397da1f84b6c
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-02-16 12:20:27 +0100

    roslaunch2: provide basic Bash completion functionality (for package names & launch modules)

--- config/roslaunch2_auto_completion.bash
+++ config/roslaunch2_auto_completion.bash
@@ -14,39 +14,33 @@ function _roscomplete_roslaunch2 {
 
     # List all supported flags in roslaunch and roslaunch2:
     roslaunch_default_flags="--files --args --nodes --find-node --child --local --screen --server_uri --run_id --wait --port --core --pid --dump-params --skip-log-check --ros-args"
-    roslaunch_meta_flags="--help --version  --no-colors --keep --dry-run --list --dir --doc"
-    roslaunch_meta_flags_short="-h -? -v -"
+    roslaunch2_flags="--help --version  --no-colors --dry-run --ros-args"
+    roslaunch2_flags_short="-h -d -v"
 
     # This implements the completion for the various token that havev already been typed:
     if [[ ${arg} =~ \-.* ]]; then # flags beginning with a dash ('-')
-        COMPREPLY=(${COMPREPLY[@]} $(compgen -W "$roslaunch_default_flags $roslaunch_meta_flags $roslaunch_meta_flags_short" -- ${arg}))
-    elif [[ ${prev} =~ \-\-doc ]]; then # --doc <FUNC> option
-        local avail_api=$(roslaunch_meta --list --no-colors | awk '{print $1;}' | cut -d\  -f1 | sort | xargs)
-        COMPREPLY=( $(compgen -W "${avail_api}" -- ${arg}) )
-    elif [[ ${prev} =~ \-\-dir ]]; then # --dir <PATH> option
-        local IFS=$'\n'
-        COMPREPLY=( $( compgen -o plusdirs -- $arg ) )
-    else # <package> <meta_launch_file>
-        _roscomplete_search_dir "( -type f -regex .*\.lmeta$ )"
+        COMPREPLY=(${COMPREPLY[@]} $(compgen -W "$roslaunch_default_flags $roslaunch2_flags $roslaunch2_flags_short" -- ${arg}))
+    else # <package> <launch_module>
+        _roscomplete_search_dir "( -type f -regex .*\.pyl$ )"
         if [[ $COMP_CWORD == 1 ]]; then
-           COMPREPLY=($(compgen -o plusdirs -f -X "!*.launch" -- ${arg}) ${COMPREPLY[@]} $(compgen -o plusdirs -f -X "!*.test" -- ${arg}) ${COMPREPLY[@]})
+           COMPREPLY=($(compgen -o plusdirs -f -X "!*.pyl" -- ${arg}) ${COMPREPLY[@]} ${COMPREPLY[@]})
         fi
-        # complete roslaunch arguments for a launch file
-        if [[ ${#COMP_WORDS[@]} -ge 2 ]]; then
-            ROSLAUNCH_COMPLETE=$(which roslaunch-complete)
-            if [[ -x ${ROSLAUNCH_COMPLETE} ]]; then
-                # Call roslaunch-complete instead of roslaunch to get arg completion
-                _roslaunch_args=$(${ROSLAUNCH_COMPLETE} ${COMP_WORDS[@]:1:2} 2> /dev/null)
-                # roslaunch-complete should be very silent about
-                # errors and return 0 if it produced usable completion.
-                if [[ $? == 0 ]]; then
-                    COMPREPLY=($(compgen -W "${_roslaunch_args}" -- "${arg}") ${COMPREPLY[@]})
-                    # FIXME maybe leave ${COMPREPLY[@]} out if we completed successfully.
-                fi
-            fi
-        fi
+        # TODO provide completion for command line args of current launch module
+#        # complete roslaunch arguments for a launch file
+#        if [[ ${#COMP_WORDS[@]} -ge 2 ]]; then
+#            ROSLAUNCH_COMPLETE=$(which roslaunch-complete)
+#            if [[ -x ${ROSLAUNCH_COMPLETE} ]]; then
+#                # Call roslaunch-complete instead of roslaunch to get arg completion
+#                _roslaunch_args=$(${ROSLAUNCH_COMPLETE} ${COMP_WORDS[@]:1:2} 2> /dev/null)
+#                # roslaunch-complete should be very silent about
+#                # errors and return 0 if it produced usable completion.
+#                if [[ $? == 0 ]]; then
+#                    COMPREPLY=($(compgen -W "${_roslaunch_args}" -- "${arg}") ${COMPREPLY[@]})
+#                fi
+#            fi
+#        fi
     fi
 }
 
-# Activate completion support for roslaunch_meta:
+# Activate completion support for roslaunch2:
 complete -F "_roscomplete_roslaunch2" -o filenames "roslaunch2"
