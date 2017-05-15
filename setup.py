2fc59a0da55283daebf935b237736941e44b257d
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-05-15 16:09:17 +0200

    roslaunch2: add roslaunch2 PyRO server code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['roslaunch2'],
     package_dir={'': 'src'},
-    scripts=['script/roslaunch2']
+    scripts=['script/roslaunch2', 'script/roslaunch2_server']
 )
 
 setup(**d)
