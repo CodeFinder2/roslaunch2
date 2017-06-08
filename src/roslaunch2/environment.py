9b8ee6646837c57ac0e7df55d9dcaf5111e7b805
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-06-08 23:33:07 +0200

    roslaunch2: support remote.Resolvable's in environment variable values
    This allows us to remotely resolve environment variables.

--- src/roslaunch2/environment.py
+++ src/roslaunch2/environment.py
@@ -2,9 +2,13 @@ import lxml.etree
 import warnings
 
 import interfaces
+import machine
 
 
 class EnvironmentVariable(interfaces.GeneratorBase, interfaces.Composable):
+    """
+    Note: this class cannot be used inside a Machine object (deprecated since ROS Fuerte).
+    """
     def __init__(self, name, value):
         interfaces.GeneratorBase.__init__(self)
         interfaces.Composable.__init__(self)
@@ -17,8 +21,10 @@ class EnvironmentVariable(interfaces.GeneratorBase, interfaces.Composable):
         if not self.rooted:
             warnings.warn('{} has been created but never add()ed.'.format(str(self)), Warning, 2)
 
-    def generate(self, root, machines, pkg):
+    def generate(self, root, starting_machine, pkg):
         elem = lxml.etree.SubElement(root, 'env')
         interfaces.GeneratorBase.to_attr(elem, 'name', self.name, str)
-        # TODO: allow self.value to be a remote.Resolvable object?
-        interfaces.GeneratorBase.to_attr(elem, 'value', self.value, str)
+        if not isinstance(starting_machine, machine.Machine):
+            starting_machine = machine.Localhost
+        self.value = machine.Machine.resolve_if(self.value, starting_machine, pkg)
+        interfaces.GeneratorBase.to_attr(elem, 'value', self.value)
