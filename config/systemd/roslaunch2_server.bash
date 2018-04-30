c9740baac6ad8b776a27246db55b72a33eab09a7
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2018-04-30 14:52:36 +0200

    roslaunch2: remove entire code from gh-pages branch (not needed there)

--- config/systemd/roslaunch2_server.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-# The special shebang line in this script is important in order to execute this in a login shell,
-# effectively sourcing the ~/.bashrc and setting up Python and ROS correctly. Since this is started
-# in a login shell, no output is visible in "sudo journalctl -ru $SERVICE_UNIT_NAME"
-
-# Run roslaunch2 server instance for remote execution:
-roslaunch2_server
